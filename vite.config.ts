32570e6804a10e2ecd812fac7eb318f9fa6f507c
Linh Dev <nguyentranlinh31@gmail.com>
2026-03-15 14:18:06 +0700

    feat: initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-    },
-  },
+  plugins: [react(), tailwindcss()],
 });
