21ca74c63374dcaea75133c7db718bb0d70cf03d
Linh Tran <nguyentranlinh31@gmail.com>
2026-03-30 10:26:48 +0700

    fix

--- src/modules/masterData/visaFastTrack/components/ProviderForm.tsx
+++ src/modules/masterData/visaFastTrack/components/ProviderForm.tsx
@@ -1,53 +1,54 @@
-import {useForm } from "react-hook-form";
+import Section from "@/shared/components/common/Section";
+import FormSelect from "@/shared/components/form/FormSelect";
+import { Button } from "@/shared/components/ui/button";
+import { Form } from "@/shared/components/ui/form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Save } from "lucide-react";
+import { useMemo } from "react";
+import { useForm } from "react-hook-form";
+import { supplierMockStore } from "../../supplier/data/supplier.mock-store";
+import { mapVisaServiceDataToFormValues } from "../mappers/visa-fast-track-form.mapper";
 import { visaServiceSchema, type VisaServiceFormValues } from "../schemas/visa-fast-track.schema";
 import type { VisaService } from "../types/visa-fast-track.type";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { mapVisaServiceDataToFormValues } from "../mappers/visa-fast-track-form.mapper";
-import FormInput from "@/shared/components/form/FormInput/FormInput";
-import { Button } from "@/shared/components/ui/button";
-import { Save } from "lucide-react";
 import VisaServiceForm from "./VisaServiceForm";
-import Section from "@/shared/components/common/Section";
-import { Form } from "@/shared/components/ui/form";
-import { useMemo } from "react";
-import { supplierMockStore } from "../../supplier/data/supplier.mock-store";
-import FormSelect from "@/shared/components/form/FormSelect";
 type ProviderFormProps = {
-     defaultValues?: VisaService | undefined;
-      onSubmit: (values: VisaServiceFormValues) => void;
-      onCancel: () => void;
-      isSubmitting?: boolean;
-      isEdit?: boolean;
-}
+  defaultValues?: VisaService | undefined;
+  onSubmit: (values: VisaServiceFormValues) => void;
+  onCancel: () => void;
+  isSubmitting?: boolean;
+  isEdit?: boolean;
+};
 
 export default function ProviderForm({ defaultValues, onSubmit, onCancel, isSubmitting, isEdit }: ProviderFormProps) {
-   const form = useForm<VisaServiceFormValues>({
-      resolver: zodResolver(visaServiceSchema),
-      defaultValues: mapVisaServiceDataToFormValues(defaultValues),
-    });
-  
-     const suppliersOptions = useMemo(() => supplierMockStore.getAll().map((supplier) => ({ label: supplier.name, value: supplier.name })), []);
-    const handleSubmit = (values) => {
-        
-      const supplier = supplierMockStore.getAll().find((s) => s.name === values.provider);
+  const form = useForm<VisaServiceFormValues>({
+    resolver: zodResolver(visaServiceSchema),
+    defaultValues: mapVisaServiceDataToFormValues(defaultValues),
+  });
 
-      const payload = {
-        ...values,
-        provider: supplier?.name,
-        country: supplier?.country,
-        city: supplier?.city
-      };
-        onSubmit(payload);}
-    return (
+  const suppliersOptions = useMemo(() => supplierMockStore.getAll().map((supplier) => ({ label: supplier.name, value: supplier.name })), []);
+  const handleSubmit = (values: any) => {
+    const supplier = supplierMockStore.getAll().find((s) => s.name === values.provider);
+
+    const payload = {
+      ...values,
+      provider: supplier?.name,
+      country: supplier?.country,
+      city: supplier?.city,
+    };
+    onSubmit(payload);
+  };
+  return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(handleSubmit)} className='space-y-6'>
         <div className='gap-4 grid grid-cols-1 sm:grid-cols-2'>
-           <FormSelect name='provider' options={suppliersOptions} label='Nhà cung cấp' required />
+          <FormSelect name='provider' options={suppliersOptions} label='Nhà cung cấp' required />
           {/* <FormInput name='code' label='Mã dịch vụ' required /> */}
           <Section title='Thông tin dịch vụ visa/fast track' className='col-span-2'>
-                <VisaServiceForm />
+            <VisaServiceForm />
           </Section>
-          {form.formState.errors.services?.message && <p className='font-normal text-destructive text-sm'>{form.formState.errors.services.message}</p>}
+          {form.formState.errors.services?.message && (
+            <p className='font-normal text-destructive text-sm'>{form.formState.errors.services.message}</p>
+          )}
         </div>
         <div className='flex justify-start gap-3'>
           <Button type='button' variant='outline' size='lg' onClick={onCancel}>
@@ -59,7 +60,6 @@ export default function ProviderForm({ defaultValues, onSubmit, onCancel, isSubm
             {isEdit ? "Cập nhật" : "Lưu"}
           </Button>
         </div>
-      
       </form>
     </Form>
   );
